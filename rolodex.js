e4663155ec76b41f9209e31b88d2a8adb0860442
Tony Schneider <tonywok@gmail.com>
2011-11-05 23:05:25 -0400

    Split top-to-middle and middle-to-bottom into seperate animations

--- rolodex.js
+++ rolodex.js
@@ -1,3 +1,9 @@
-function Rolodex(options) {
-  this.lapse = options.initTime;
-}
+$(function() {
+  // silly function so I don't have to manually test
+  function doit() {
+    $('#rolodex-seconds-top-0').addClass('top-to-middle');
+    $('#rolodex-seconds-bottom-8').addClass('middle-to-bottom');
+  };
+
+  doit();
+});
