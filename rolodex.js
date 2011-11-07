8241b60f87842aa139589624e40a28693ac1ba9b
Tony Schneider <tonywok@gmail.com>
2011-11-06 20:12:19 -0500

    *sort of* working with 2 numbers

--- rolodex.js
+++ rolodex.js
@@ -1,9 +1,43 @@
 $(function() {
-  // silly function so I don't have to manually test
-  function doit() {
-    $('#rolodex-seconds-top-0').addClass('top-to-middle');
-    $('#rolodex-seconds-bottom-8').addClass('middle-to-bottom');
-  };
 
-  doit();
+  var Rolodex = (function(rolodex) {
+
+    var cssEvents = { flip : 'top-to-middle', flop : 'middle-to-bottom' },
+        count   = 0,
+        numbers = [[0,8], [8,0]],
+        top, bottom;
+
+    var run = function() {
+      var pos = count % 2;
+      top     = numbers[pos][0],
+      bottom  = numbers[pos][1];
+
+      $('#rolodex-seconds-top-' + top).addClass('top-to-middle');
+      $('#rolodex-seconds-bottom-' + bottom).addClass('middle-to-bottom');
+      count++;
+    };
+
+    rolodex.init = function(options) {
+      // $(".number").bind("webkitAnimationStart", function(e) {
+      //   var event = e.originalEvent;
+      // });
+
+      $(".number").bind("webkitAnimationEnd", function(e) {
+        var event = e.originalEvent;
+
+        if ( event.animationName == 'middle-to-bottom' ) {
+          $('#rolodex-seconds-top-' + top).removeClass('top-to-middle');
+          $('#rolodex-seconds-bottom-' + bottom).removeClass('middle-to-bottom');
+        }
+      });
+
+      setInterval(run, 1000);
+    };
+
+    return rolodex;
+
+  })({});
+
+  Rolodex.init();
+
 });
