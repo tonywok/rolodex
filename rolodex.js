63f03c3a5b355c6027187c89d8c75c88bc2810c8
Andrew Grieser + Tony Schneider <pairing+ag+ts@gmail.com>
2011-11-09 19:08:38 -0500

    checkpoint

--- rolodex.js
+++ rolodex.js
@@ -1,37 +1,47 @@
 jQuery(function($) {
 
   var Rolodex = (function(rolodex) {
-
     var cssEvents = { flip : 'top-to-middle', flop : 'middle-to-bottom' },
-        count   = 0,
-        numbers = [[0,8], [8,0]],
-        top, bottom;
+        index   = 0, next;
 
-    var run = function() {
-      var pos = count % 2;
-      top     = numbers[pos][0],
-      bottom  = numbers[pos][1];
+    rolodex.advance = function() {
+      next = (index + 1) % 10;
 
-      $('#rolodex-seconds-top-' + top).addClass('top-to-middle');
-      $('#rolodex-seconds-bottom-' + bottom).addClass('middle-to-bottom');
-      count++;
-    };
+      // remove some classes!
+      $("img.top-current").removeClass('top-current');
+      $("img.bottom-current").removeClass('bottom-current');
 
-    rolodex.init = function(options) {
+      $("img.top-next").removeClass("top-next").addClass("top-current");
+      $("img.bottom-next").removeClass("bottom-next").addClass("bottom-current");
+
+      /* $("#rolodex-top-" + index).addClass("top-current"); */
+      $("#rolodex-top-" + next).addClass("top-next");
+      /* $("#rolodex-bottom-" + index).addClass("bottom-current"); */
+      $("#rolodex-bottom-" + next).addClass("bottom-next");
+
+      index = next;
+    }
+
       // $(".number").bind("webkitAnimationStart", function(e) {
       //   var event = e.originalEvent;
       // });
+    rolodex.init = function(options) {
+      index = 0;
 
       $(".number").bind("webkitAnimationEnd", function(e) {
         var event = e.originalEvent;
 
-        if ( event.animationName == 'middle-to-bottom' ) {
-          $('#rolodex-seconds-top-' + top).removeClass('top-to-middle');
-          $('#rolodex-seconds-bottom-' + bottom).removeClass('middle-to-bottom');
+        if ( event.animationName === 'middle-to-bottom' ) {
+          // $('#rolodex-top-' + top).removeClass('top-current');
+          // $('#rolodex-bottom-' + bottom).removeClass('bottom-previous');
+
+          // $('#rolodex-top-' + top).removeClass('top-to-middle');
+          // $('#rolodex-bottom-' + bottom).removeClass('middle-to-bottom');
+          $('
         }
       });
 
-      setInterval(run, 1000);
+      setInterval(advance, 1000);
     };
 
     return rolodex;
